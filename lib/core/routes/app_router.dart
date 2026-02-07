e216d4cd536ed3ce169baa7971db61f2357707a2
Artenlaclase <artenlaclase@gmail.com>
2026-02-07 13:33:34 -0300

    Refactor error message layout in AppRouter for improved readability

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -8,9 +8,7 @@ class AppRouter {
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
-            body: Center(
-              child: Text('No route defined for ${settings.name}'),
-            ),
+            body: Center(child: Text('No route defined for ${settings.name}')),
           ),
         );
     }
